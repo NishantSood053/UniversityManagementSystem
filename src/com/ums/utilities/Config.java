0f736498a67edca83c4a9467b54a9cbb18783563
Nishant <nsood053@uottawa.ca>
2017-10-04 15:24:24 -0400

    Change Properties for Log4J
    -To support macOSX file system

--- src/com/ums/utilities/Config.java
+++ src/com/ums/utilities/Config.java
@@ -2,7 +2,7 @@ package com.ums.utilities;
 
 public class Config {
 	public static final int MAX_CLIENTS = 5;
-	public static final int DEFAULT_PORT = 1050;
+	public static final int DEFAULT_PORT = 5050;
 	public static final String DEFAULT_HOST = "127.0.0.1";
 	public static final int MAX_BORROWED_ITEMS = 3;
 	//20 seconds for 1 day
